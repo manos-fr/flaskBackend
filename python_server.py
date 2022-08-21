3b5d9c4af9292ff2924ee9d86de61aea306abe6e
Manos <m.kakarakis@doubleip.com>
2022-08-21 21:42:13 +0300

    change port

--- python_server.py
+++ python_server.py
@@ -131,4 +131,4 @@ def update_title(id):
 if __name__ == '__main__':
     connection = psycopg2.connect(user="vqlnioqfkvocxj", password="20de6fb3a0fd8e652444adbe7273c89429f28c11cc1376e46eb789cfb8474bef",
                                   host="ec2-54-229-217-195.eu-west-1.compute.amazonaws.com", port="5432", database="de9mikemijit5j")
-    app.run(host="0.0.0.0", port=3000, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
